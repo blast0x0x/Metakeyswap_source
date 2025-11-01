3233deb50b90e01683c7077e7318c2bbb262c3bf
blast0x0x <alphamate0918@gmail.com>
2025-11-02 01:32:15 +0900

    update

--- frontend/src/components/TransactionConfirmationModal/index.tsx
+++ frontend/src/components/TransactionConfirmationModal/index.tsx
@@ -37,9 +37,9 @@ function ConfirmationPendingContent({ pendingText }: { pendingText: string }) {
   const { t } = useTranslation()
   return (
     <Wrapper>
-      <ConfirmedIcon>
+      {/* <ConfirmedIcon>
         <Spinner />
-      </ConfirmedIcon>
+      </ConfirmedIcon> */}
       <AutoColumn gap="12px" justify="center">
         <Text fontSize="20px">{t('Waiting For Confirmation')}</Text>
         <AutoColumn gap="12px" justify="center">
